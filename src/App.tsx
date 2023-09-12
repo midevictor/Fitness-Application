a5e872b5ee9b17e590243cd990e1a4a9793dc9bf
Mide Victor <midevictor90@gmail.com>
2023-09-12 17:42:28 +0100

    created and implemented the responsive navbar for all screens

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,28 @@
+import { useEffect, useState } from "react"
+import NavBar from '@/scenes/Navbar'
+import { SelectedPage } from "./scenes/shared/type"; 
+
 function App() {
+  const [selectedPage, setSelectedPage] = useState<SelectedPage>(SelectedPage.Home);
+  const [isTopOfPage, setIsTopOfPage] = useState<boolean>(true);
+
+  useEffect(()=> {
+    const handleScroll = () => {
+      if(window.scrollY === 0){
+        setIsTopOfPage(true);
+        setSelectedPage(SelectedPage.Home)
+      }
+      else{
+        setIsTopOfPage(false);
+      }
+
+       }
+       window.addEventListener("scroll", handleScroll);
+       return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
   return (
     <div className="bg-gray-20">
-      App
+      <NavBar isTopOfPage={isTopOfPage} selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
     </div>
   )
 }
---
+++ src/scenes/Navbar/Link.tsx
@@ -0,0 +1,23 @@
+import AnchorLink from "react-anchor-link-smooth-scroll"
+import { SelectedPage } from "../shared/type";
+//anytime you pass a prop, always define what type it is
+type Props = {
+  page: string;
+  selectedPage: SelectedPage;
+  setSelectedPage: (value: SelectedPage) => void;
+}
+
+const Link = ({page, selectedPage, setSelectedPage}: Props) => {
+  const lowerCasePage = page.toLowerCase().trim() as SelectedPage
+  return (
+    <AnchorLink 
+    className={`${selectedPage === lowerCasePage ? "text-primary-500" : "" } transition duration-500 hover:text-primary-300 ` }
+    href={`#${lowerCasePage}`}
+    onClick={()=> setSelectedPage(lowerCasePage)}
+    >
+      {page}
+    </AnchorLink>
+  )
+}
+
+export default Link
